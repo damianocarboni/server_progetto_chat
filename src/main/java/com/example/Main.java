49fa70a4ae137209843b06113aee90d0b2602418
lai2-24 <lai2-24@itismeucci.com>
2024-11-05 14:29:43 +0100

    secondo commit

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -1,7 +1,23 @@
 package com.example;
 
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
+
+public class Main extends IOException {
+    public static void main(String[] args) throws IOException {
+        System.out.println("server avviato");
+        ArrayList<MioThread> Threads = new ArrayList<MioThread>();
+        ArrayList<String> NomiScelti = new ArrayList<String>();
+        ServerSocket s1 = new ServerSocket(3000);
+        
+
+        do{
+            Socket s = s1.accept();
+            System.out.println("un client si è collegato");
+            MioThread t = new MioThread(s, Threads, NomiScelti);
+            t.start();
+        } while(true);
     }
 }
--- src/main/java/com/example/MioThread.java
+++ src/main/java/com/example/MioThread.java
@@ -1,5 +1,64 @@
 package com.example;
 
-public class MioThread extends Thread{
-    
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.Socket;
+import java.util.ArrayList;
+
+public class MioThread extends Thread {
+
+    Socket s = new Socket();
+    ArrayList<MioThread> threads;
+    BufferedReader in;
+    DataOutputStream out;
+    ArrayList<String> NomiScelti;
+    String nome;
+
+    public MioThread(Socket s, ArrayList threads, ArrayList NomiScelti) throws IOException {
+        this.s = s;
+        this.threads = threads;
+        this.NomiScelti = NomiScelti;
+        in = new BufferedReader(new InputStreamReader(s.getInputStream()));
+        out = new DataOutputStream(s.getOutputStream());
+    }
+
+    public boolean verificaNome() throws IOException {
+        for (int i = 0; i < NomiScelti.size(); i++) {
+            if (nome.equals(NomiScelti.get(i))) {
+                out.writeBytes("!Username già in uso\n");
+                return true;
+            }
+        }
+        NomiScelti.add(nome);
+        return false;
+    }
+
+    public void comunicaJoin() throws IOException {
+        for (int i = 0; i < threads.size(); i++) {
+            threads.get(i).sendMessage("Join:" + nome);
+        }
+    }
+
+    public void run() {
+        try {
+
+            do {
+                out.writeBytes("Inserisci lo username:\n");
+                nome = in.readLine();
+            } while (verificaNome());
+            comunicaJoin();
+            threads.add(this);
+
+            s.close();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void sendMessage(String msg) throws IOException{
+        out.writeBytes(msg + "\n");
+    }
 }
